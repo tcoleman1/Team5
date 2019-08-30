ff3ecdf75c1ba55db8cb0285f73e0bc60b8eb2b7
jastring <tripp.stringfield@gmail.com>
2019-08-30 15:56:48 -0400

    zomato api functionality working

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,19 +1,81 @@
 
-
-var apiKey = "caf17b1dfec1bc4c754bb5ebed865557" // zomato api key 
-
-$("#submit-button").on("click", function () {
-
-    var destinationInput = $("#destination-input").val()
-
-    var queryUrl = 'https://developers.zomato.com/api/v2.1/search?entity_id=charlotte&entity_type=city'
+function zomatoGetCity(e) {
+    var city = $("#destination-input").val().trim();
 
     $.ajax({
+        type: "GET",
+        dataType: 'json',
+        url: 'https://developers.zomato.com/api/v2.1/cities?q=' + city,
+        headers: {
+            'user-key': 'caf17b1dfec1bc4c754bb5ebed865557'
+        },
+        success: function (data) {
+            console.log(data);
+            var cityID = data.location_suggestions[0].id;
+            zomatoGetRestaurants(cityID)
+        },
+        error: function (xhr, status, err) {
+        }
+    });
+    e.preventDefault();
 
+}
+function zomatoGetRestaurants(a) {
+    var id = a;
+    $.ajax({
+        type: "GET",
+        dataType: 'json',
+        url: "https://developers.zomato.com/api/v2.1/search?entity_id=" + id + "&entity_type=city",
+        headers: {
+            'user-key': 'caf17b1dfec1bc4c754bb5ebed865557'
+        },
+        success: function (data) {
+            console.log(data);
+             var z=data.restaurants;
+            $("#table-head").empty();
+            $("#table-head").append("<th scope='col'>Restaurant Name</th><th scope='col'>Type</th><th scope='col'>Rating</th><th scope='col'></th>");
+            $("#table-body").empty();
+            for (var i = 0; i < z.length; i++) {
+                $("#table-body").append("<tr>" + "<td>" + z[i].restaurant.name + "</td><td>" + z[i].restaurant.cuisines + "</td><td>"
+                    + z[i].restaurant.user_rating.aggregate_rating + "</td><td><button type='button' class='btn btn-success zomato' data-toggle='modal' data-target='exampleModal' id=" + z[i].restaurant.id + ">More Info</button></td></tr>");
+            }
+        },
+        error: function (xhr, status, err) {
+        }
+    });
+}
+function zomatoModal(e) {
+    var id = $(this).attr('id');
+    console.log(id);
+    $.ajax({
+        type: "GET",
+        dataType: 'json',
+        url: "https://developers.zomato.com/api/v2.1/restaurant?res_id=" + id, 
+        headers: {
+            'user-key': 'caf17b1dfec1bc4c754bb5ebed865557'
+        },
+        success: function (data) {
+            var z = data;
+            $(".modal-body").empty();
+            var modalImage = $("<img><br>")
+            modalImage.attr("src", z.featured_image);
+            modalImage.attr("height", "200px");
+            modalPhone = $("<h5 style='text-align:center'>");
+            modalAddress = $("<h5 style='text-align:center'>");
+            modalAddress.text(z.location.address);
+            modalPhone.text(z.phone_numbers);
+            $(".modal-body").append(modalImage);
+            $(".modal-body").append(modalPhone);
+            $(".modal-body").append(modalAddress);
+            $("#exampleModalLabel").text(z.name);
+            $("#exampleModal").modal();
+        },
+        error: function (xhr, status, err) {
+        }
+    });
+    e.preventDefault();
+}
 
-
-    })
-})
 function ticketMaster(e) {
     var city = $("#destination-input").val().trim();
     var startDate = $("#depart-input").val();
@@ -21,7 +83,7 @@ function ticketMaster(e) {
     $.ajax({
         type: "GET",
         url: "https://app.ticketmaster.com/discovery/v2/events.json?apikey=YlU1Z6st1DDtdKQahLrwevvAJXCU3LXr&city="
-         + city + "&startDateTime=" + startDate + "T00:00:00Z&endDateTime=" + endDate + "T00:00:00Z",//need to change to 11:59:59Z
+            + city + "&startDateTime=" + startDate + "T00:00:00Z&endDateTime=" + endDate + "T00:00:00Z",//need to change to 11:59:59Z
         async: true,
         dataType: "json",
         success: function (data) {
@@ -31,7 +93,7 @@ function ticketMaster(e) {
             $("#table-body").empty();
             for (var i = 0; i < tm.length; i++) {
                 $("#table-body").append("<tr>" + "<td>" + tm[i].name + "</td><td>" + tm[i].dates.start.localDate + "</td><td>"
-                 + tm[i].dates.start.localTime + "</td><td><button type='button' class='btn btn-success' data-toggle='modal' data-target='exampleModal' id=" + tm[i].id + ">More Info</button></td></tr>");
+                    + tm[i].dates.start.localTime + "</td><td><button type='button' class='btn btn-success ticket' data-toggle='modal' data-target='exampleModal' id=" + tm[i].id + ">More Info</button></td></tr>");
             }
         },
         error: function (xhr, status, err) {
@@ -39,7 +101,7 @@ function ticketMaster(e) {
     });
     e.preventDefault();
 }
-function ticketMasterModal(e){
+function ticketMasterModal(e) {
     var id = $(this).attr('id');
     console.log(id);
     $.ajax({
@@ -70,7 +132,9 @@ function ticketMasterModal(e){
 
 }
 $("#tm-button").on("click", ticketMaster);
-$(document).on("click", ".btn-success", ticketMasterModal);
+$("#restaurant-button").on("click", zomatoGetCity);
+$(document).on("click", ".ticket", ticketMasterModal);
+$(document).on("click", ".zomato", zomatoModal);
 
 
 
